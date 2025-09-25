23f06285c84e68a2886131510e20be62d62caa66
saturnell <kingazr958@gmail.com>
2025-09-25 09:03:19 +0700

    second

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/wedding-invitation-santi-mantri",
+  base: "/web-wedding",
 });
